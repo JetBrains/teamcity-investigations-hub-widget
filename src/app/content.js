54d755b36b27a5b69da36752cc7a8454b47ef682
Maxim Mazin <maxim.mazin@jetbrains.com>
2018-08-20 14:04:47 +0300

    Fixed widget content lint errors

--- src/app/content.js
+++ src/app/content.js
@@ -6,33 +6,44 @@ import Link from '@jetbrains/ring-ui/components/link/link';
 import styles from './app.css';
 import EmptyWidget from './empty-widget';
 
-const renderNotConfiguredService = onConfigure => (
-  <span>
-    {'TeamCity service to get investigations from, it not configuration yet. Please, '}
-    <Link
-      onClick={onConfigure}
-    >{'select the service.'}</Link>
-  </span>
-);
+function WidgetContent({children}) {
+  return (
+    <div className={styles.widget}>
+      {children}
+    </div>
+  );
+}
 
-const renderEmpty = () => (
-  <EmptyWidget header={'(⌒‿⌒)'}>
-    {'No investigations'}
-    <br/>
-    {'are assigned to you'}
-  </EmptyWidget>
-);
+WidgetContent.propTypes = {
+  children: PropTypes.node
+};
 
-const Content = ({teamcityService, investigations, onConfigure}) => (
-  <div className={styles.widget}>
-    {!teamcityService
-      ? renderNotConfiguredService(onConfigure)
-      : investigations.length === 0
-        ? renderEmpty()
-        : teamcityService.name
-    }
-  </div>
-);
+const Content = ({teamcityService, investigations, onConfigure}) => {
+  if (!teamcityService) {
+    return (
+      <WidgetContent>
+        <span>
+          {'TeamCity service to get investigations from, it not configuration yet. Please, '}
+          <Link onClick={onConfigure}>{'select the service.'}</Link>
+        </span>
+      </WidgetContent>
+    );
+  } else if (!investigations.length) {
+    return (
+      <WidgetContent>
+        <EmptyWidget header={'(⌒‿⌒)'}>
+          {'No investigations'}
+          <br/>
+          {'are assigned to you'}
+        </EmptyWidget>
+      </WidgetContent>
+    );
+  } else {
+    return (
+      <WidgetContent>{teamcityService.name}</WidgetContent>
+    );
+  }
+};
 
 Content.propTypes = {
   teamcityService: PropTypes.object,
