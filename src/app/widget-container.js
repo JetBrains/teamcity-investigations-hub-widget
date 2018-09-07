ca367e6fbfe000fa823a1b1df8d21cb7c0dd9b15
Maxim Mazin <maxim.mazin@jetbrains.com>
2018-09-07 14:38:17 +0200

    Got rid of widget-title-container.js

--- src/app/widget-container.js
+++ src/app/widget-container.js
@@ -1,12 +1,30 @@
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 
+import {i18n} from 'hub-dashboard-addons/dist/localization';
+
 import Widget from './widget';
 
 const WidgetContainer = connect(
   (state, {dashboardApi}) => ({
     dashboardApi,
-    isConfiguring: state.configuration.isConfiguring
+    isConfiguring: state.configuration.isConfiguring,
+    title: state.configuration.isConfiguring
+      ? {
+        title: i18n('TeamCity Investigations'),
+        counter: -1,
+        href: null,
+        dashboardApi
+      }
+      : {
+        title: i18n('TeamCity Investigations'),
+        counter: state.investigationsCount,
+        href: state.teamcityService &&
+          state.teamcityService.homeUrl &&
+          `${state.teamcityService.homeUrl}/investigations.html`,
+        dashboardApi
+      }
+
   }))(Widget);
 
 WidgetContainer.propTypes = {
--- src/app/widget.js
+++ src/app/widget.js
@@ -3,21 +3,32 @@ import PropTypes from 'prop-types';
 
 import ContentContainer from './content-container';
 import ConfigurationContainer from './configuration-container';
-import WidgetTitleContainer from './widget-title-container';
+import WidgetTitle from './lib/widget-title/widget-title';
 
-const Widget = ({isConfiguring, dashboardApi}) => (
-  <div>
-    <WidgetTitleContainer dashboardApi={dashboardApi}/>
-    {isConfiguring ? (
-      <ConfigurationContainer/>
-    ) : (
-      <ContentContainer/>
-    )}
-  </div>
-);
+const Widget = (
+  {
+    isConfiguring,
+    title,
+    dashboardApi
+  }) =>
+  (
+    <div>
+      <WidgetTitle {...title} dashboardApi={dashboardApi}/>
+      {isConfiguring ? (
+        <ConfigurationContainer/>
+      ) : (
+        <ContentContainer/>
+      )}
+    </div>
+  );
 
 Widget.propTypes = {
   isConfiguring: PropTypes.bool.isRequired,
+  title: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    counter: PropTypes.number,
+    href: PropTypes.string
+  }).isRequired,
   dashboardApi: PropTypes.object.isRequired
 };
 
