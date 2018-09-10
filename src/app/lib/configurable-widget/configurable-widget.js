a6bf6c5e69a99fa655ef3a523e7aacfa6caf281e
Maxim Mazin <maxim.mazin@jetbrains.com>
2018-09-10 15:25:40 +0200

    ConfigurableWidget as a function

--- src/app/lib/configurable-widget/configurable-widget.js
+++ src/app/lib/configurable-widget/configurable-widget.js
@@ -1,40 +1,29 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import ConfigurationMode from './configuration-mode';
 
 
-class ConfigurableWidget extends PureComponent {
+const ConfigurableWidget = ({isConfiguring, Title, Configuration, Content, dashboardApi}) => (
+  <div>
+    <ConfigurationMode
+      isConfiguring={isConfiguring}
+      dashboardApi={dashboardApi}
+    />
+    {<Title dashboardApi={dashboardApi}/>}
+    {isConfiguring ? <Configuration/> : <Content/>}
+  </div>
+);
 
-  static propTypes = {
-    isConfiguring: PropTypes.bool.isRequired,
-    dashboardApi: PropTypes.object.isRequired,
-    // React component
-    Title: PropTypes.func,
-    // React component
-    Configuration: PropTypes.func.isRequired,
-    // React component
-    Content: PropTypes.func.isRequired
-  };
-
-  render() {
-    const {isConfiguring, Title, Configuration, Content, dashboardApi} = this.props;
-
-    return (
-      <div>
-        <ConfigurationMode
-          isConfiguring={isConfiguring}
-          dashboardApi={dashboardApi}
-        />
-        {<Title dashboardApi={dashboardApi}/>}
-        {isConfiguring ? (
-          <Configuration/>
-        ) : (
-          <Content/>
-        )}
-      </div>
-    );
-  }
-}
+ConfigurableWidget.propTypes = {
+  isConfiguring: PropTypes.bool.isRequired,
+  dashboardApi: PropTypes.object.isRequired,
+  // React component
+  Title: PropTypes.func,
+  // React component
+  Configuration: PropTypes.func.isRequired,
+  // React component
+  Content: PropTypes.func.isRequired
+};
 
 export default ConfigurableWidget;
