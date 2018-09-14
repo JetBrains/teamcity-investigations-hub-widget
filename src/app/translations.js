2636773d8d25f24fc26b29f22aee73304fd46f15
katriyna <katriyna@gmail.com>
2018-09-14 14:17:27 +0300

    map translations keys after po-files are imported

--- src/app/translations.js
+++ src/app/translations.js
@@ -1,7 +1,13 @@
-import {de} from './translations/teamcity-investigations-hub-widget_de.po';
-import {es} from './translations/teamcity-investigations-hub-widget_es.po';
-import {fr} from './translations/teamcity-investigations-hub-widget_fr.po';
-import {ru} from './translations/teamcity-investigations-hub-widget_ru.po';
-import {ja} from './translations/teamcity-investigations-hub-widget_ja.po';
+import de from './translations/teamcity-investigations-hub-widget_de.po';
+import es from './translations/teamcity-investigations-hub-widget_es.po';
+import fr from './translations/teamcity-investigations-hub-widget_fr.po';
+import ru from './translations/teamcity-investigations-hub-widget_ru.po';
+import ja from './translations/teamcity-investigations-hub-widget_ja.po';
 
-export default {de, es, fr, ru, ja};
+export default {
+  de: de.de_DE,
+  es: es.es_ES,
+  fr: fr.fr_FR,
+  ru: ru.ru_RU,
+  ja: ja.ja_JP
+};
